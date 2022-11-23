abe3a3aca6f5ff9ce485256e109ca1f68a7f5867
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-11-23 13:08:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -252,6 +252,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❤️‍🔥 ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ ❤️‍🔥', url='https://t.me/ML_LINKS_01') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
