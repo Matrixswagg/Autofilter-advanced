bf331f0daba9a6dd7e6b78989fb127279ac4a0b5
Matrixswagg <123743469+Matrixswagg@users.noreply.github.com>
2023-03-11 19:42:57 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,11 +1,11 @@
 import pymongo
 from pyrogram import enums 
-from info import DATABASE_URI, DATABASE_NAME
+from info import DATABASE_URI, DATABASE_URI2, DATABASE_NAME, DATABASE_NAME2
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
+myclient = pymongo.MongoClient(DATABASE_URI,DATABASE_URI2)
 mydb = myclient["ManualFilters"]
 
 
